f15c361a6c58ac98fa97b1d68505e9d262bb66ae
riandroraul <riandroraul@gmail.com>
2024-06-25 19:51:32 +0700

    update

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+var expressLayouts = require("express-ejs-layouts");
+const router = require("./src/Routes/main-route");
 
+dotenv.config();
+require("./src/config/db-connect");
+const port = process.env.PORT;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, "public", "assets")));
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
